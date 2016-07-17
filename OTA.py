2a53224a1f29afac3e36c446ad108e73c963e499
吴恩宇 <wuenyu007@sina.com>
2016-07-17 12:16:50 +0800

    Add files via upload
    For the use of OTA

---
+++ OTA.py
@@ -0,0 +1,322 @@
+﻿# -*- coding:utf-8 -*-
+
+def convert2dic(f):
+	"""load the file and convert it into dic,key is query word,and the values are pv,style,click,cost"""
+	#定义字典，依次存储值为PV，样式，点击，消耗
+	dic = {}
+	fileError = 0
+	for line in f:
+		line = line.strip()
+		#定义数据项为7、6及其他时的字典操作
+		if check_line_item(line) == 7:
+			PV_ID,Account_ID,KeyWord,QueryWord,Style_ID,Click,Cost = line.split('\t')
+		elif check_line_item(line) == 6:
+			PV_ID,Account_ID,KeyWord,QueryWord,Style_ID,Click,Cost = repair_line_6(line)
+		else:
+			fileError += 1
+
+		if dic.has_key(QueryWord):
+			dic[QueryWord][0] += 1
+			if dic[QueryWord][1] != Style_ID:
+				print QueryWord,'doesn''t have the same styid'
+			dic[QueryWord][2] += int(Click)
+			dic[QueryWord][3] += int(Cost)
+		else:
+			dic[QueryWord] = []
+			dic[QueryWord].append(1)
+			dic[QueryWord].append(Style_ID)
+			dic[QueryWord].append(int(Click))
+			dic[QueryWord].append(int(Cost))
+	print "origin file has %d mistakes with 7 item enough"%fileError
+	return dic
+
+def check_line_item(line):
+	"""judge the item whether if is 7"""
+	return len(line.split('\t'))
+
+def repair_line_6(line):
+	PV_ID,Account_ID,X,Y,Click,Cost = line.split('\t')
+	if check_number(Y):
+		Style_ID = int(Y)
+		KeyWord,QueryWord = split_question(X)
+		return PV_ID,Account_ID,KeyWord,QueryWord,Style_ID,Click,Cost
+	else:
+		KeyWord = X
+		QueryWord,Style_ID = split_question(Y)
+		return PV_ID,Account_ID,KeyWord,QueryWord,Style_ID,Click,Cost
+
+def check_number(s):
+	try:
+		return int(s)
+	except ValueError:
+		return None
+
+def split_question(s):
+	'''split the string with the last question mark into 2 parts'''
+	mark = 0
+	qposition =0
+	for ch in s:
+		if ch == '?':
+			qposition = mark
+		mark += 1
+	former = s[:qposition]
+	latter = s[qposition+1:]
+	return former,latter
+
+#求两个字典的交集，实现vlookup的功能
+def dic_intersection(a,b):
+	c = {}
+	for key in a:
+		if b.has_key(key):
+			#存储数据位实验组PV0,样式1，点击2，消耗3，对照组PV4，样式5，点击6，消耗7
+			c[key] = (a[key][0],a[key][1],a[key][2],a[key][3],b[key][0],b[key][1],b[key][2],b[key][3])
+	return c
+
+#基于sytle_ID输出总结数据
+def calculate_result(dic):
+	result = {}
+	for key in dic:
+		if dic[key][1] == dic[key][5]:
+			if result.has_key(dic[key][1]):
+				result[dic[key][1]][0] += dic[key][0]
+				result[dic[key][1]][1] += dic[key][2]
+				result[dic[key][1]][2] += dic[key][3]
+				result[dic[key][1]][3] += dic[key][4]
+				result[dic[key][1]][4] += dic[key][6]
+				result[dic[key][1]][5] += dic[key][7]
+			else:
+				result[dic[key][1]] = [dic[key][0],dic[key][2],dic[key][3]]
+				result[dic[key][1]].append(dic[key][4])
+				result[dic[key][1]].append(dic[key][6])
+				result[dic[key][1]].append(dic[key][7])
+		else:
+			print "Wrong,same query word's style doesn't match"
+			#打开以下语句，寻找对应问题query词
+			#print dic[key],key
+	#上侧的结果是样式：实验组PV，点击，消耗，对照组PV，点击，消耗
+	
+	#总数据
+	ExpPV = 0
+	ExpClick = 0
+	ExpCost = 0
+	ComPV = 0
+	ComClick = 0
+	ComCost = 0
+	
+	#酒店数据
+	Hotel_ExpPV = 0
+	Hotel_ExpClick = 0
+	Hotel_ExpCost = 0
+	Hotel_ComPV = 0
+	Hotel_ComClick = 0
+	Hotel_ComCost = 0
+	
+	#旅游数据
+	Travel_ExpPV = 0
+	Travel_ExpClick = 0
+	Travel_ExpCost = 0
+	Travel_ComPV = 0
+	Travel_ComClick = 0
+	Travel_ComCost = 0
+	
+	#机票数据
+	Ticket_ExpPV = 0
+	Ticket_ExpClick = 0
+	Ticket_ExpCost = 0
+	Ticket_ComPV = 0
+	Ticket_ComClick = 0
+	Ticket_ComCost = 0
+	#基于样式汇总
+	for key in result:
+		ExpPV += result[key][0]
+		ExpClick += result[key][1]
+		ExpCost += result[key][2]
+		ComPV += result[key][3]
+		ComClick += result[key][4]
+		ComCost += result[key][5]
+		
+		result[key].append(float(result[key][1])/result[key][0])
+		result[key].append(float(result[key][4])/result[key][3])
+		result[key].append(result[key][6]/result[key][7]-1)
+		
+		#汇总酒店
+		if key == "11" or key == "12"or key == "13"or key == "14":
+			Hotel_ExpPV += result[key][0]
+			Hotel_ExpClick += result[key][1]
+			Hotel_ExpCost += result[key][2]
+			Hotel_ComPV += result[key][3]
+			Hotel_ComClick += result[key][4]
+			Hotel_ComCost += result[key][5]
+		#汇总旅游
+		if key == "21" or key == "22":
+			Travel_ExpPV += result[key][0]
+			Travel_ExpClick += result[key][1]
+			Travel_ExpCost += result[key][2]
+			Travel_ComPV += result[key][3]
+			Travel_ComClick += result[key][4]
+			Travel_ComCost += result[key][5]
+		#汇总机票
+		if key == "31" or key == "32":
+			Ticket_ExpPV += result[key][0]
+			Ticket_ExpClick += result[key][1]
+			Ticket_ExpCost += result[key][2]
+			Ticket_ComPV += result[key][3]
+			Ticket_ComClick += result[key][4]
+			Ticket_ComCost += result[key][5]
+
+	ExpCTR =float(ExpClick) / ExpPV
+	ComCTR =float(ComClick) / ComPV
+	CTR_change =ExpCTR / ComCTR - 1
+	result['99'] = [ExpPV,ExpClick,ExpCost,ComPV,ComClick,ComCost,ExpCTR,ComCTR,CTR_change]
+	
+	Hotel_ExpCTR =float(Hotel_ExpClick) / Hotel_ExpPV
+	Hotel_ComCTR =float(Hotel_ComClick) / Hotel_ComPV
+	Hotel_CTR_change =Hotel_ExpCTR / Hotel_ComCTR - 1
+	result['97'] = [Hotel_ExpPV,Hotel_ExpClick,Hotel_ExpCost,Hotel_ComPV,Hotel_ComClick,Hotel_ComCost,Hotel_ExpCTR,Hotel_ComCTR,Hotel_CTR_change]
+	
+	Travel_ExpCTR =float(Travel_ExpClick) / Travel_ExpPV
+	Travel_ComCTR =float(Travel_ComClick) / Travel_ComPV
+	Travel_CTR_change =Travel_ExpCTR / Travel_ComCTR - 1
+	result['98'] = [Travel_ExpPV,Travel_ExpClick,Travel_ExpCost,Travel_ComPV,Travel_ComClick,Travel_ComCost,Travel_ExpCTR,Travel_ComCTR,Travel_CTR_change]
+	
+	if Ticket_ExpPV != 0 and Ticket_ComPV != 0:
+		Ticket_ExpCTR =float(Ticket_ExpClick) / Ticket_ExpPV
+		Ticket_ComCTR =float(Ticket_ComClick) / Ticket_ComPV
+		Ticket_CTR_change =Ticket_ExpCTR / Ticket_ComCTR - 1
+		result['96'] = [Ticket_ExpPV,Ticket_ExpClick,Ticket_ExpCost,Ticket_ComPV,Ticket_ComClick,Ticket_ComCost,Ticket_ExpCTR,Ticket_ComCTR,Ticket_CTR_change]
+	
+	return result
+
+def write_origin(fA,fB,sheet,x,y):
+	#写实验组表头
+	row = x
+	sheet.write(row,y,'PV_ID')
+	sheet.write(row,y+1,'Account_ID')
+	sheet.write(row,y+2,'KeyWord')
+	sheet.write(row,y+3,'QueryWord')
+	sheet.write(row,y+4,'Style_ID')
+	sheet.write(row,y+5,'Click')
+	sheet.write(row,y+6,'Cost')
+	#写实验组数据
+	for line in fA:
+		row += 1
+		line = line.strip()
+		if check_line_item(line) == 7:
+			PV_ID,Account_ID,KeyWord,QueryWord,Style_ID,Click,Cost = line.split('\t')
+		elif check_line_item(line) == 6:
+			PV_ID,Account_ID,KeyWord,QueryWord,Style_ID,Click,Cost = repair_line_6(line)
+		sheet.write(row,y,PV_ID)
+		sheet.write(row,y+1,Account_ID)
+		sheet.write(row,y+2,KeyWord.decode('gbk', "ignore"))
+		sheet.write(row,y+3,QueryWord.decode('gbk', "ignore"))
+		sheet.write(row,y+4,Style_ID)
+		sheet.write(row,y+5,int(Click))
+		sheet.write(row,y+6,int(Cost))
+	#写对照组表头
+	row = x
+	sheet.write(row,y+8,'PV_ID')
+	sheet.write(row,y+9,'Account_ID')
+	sheet.write(row,y+10,'KeyWord')
+	sheet.write(row,y+11,'QueryWord')
+	sheet.write(row,y+12,'Style_ID')
+	sheet.write(row,y+13,'Click')
+	sheet.write(row,y+14,'Cost')
+	#写对照组数据
+	for line in fB:
+		row += 1
+		line = line.strip()
+		if check_line_item(line) == 7:
+			PV_ID,Account_ID,KeyWord,QueryWord,Style_ID,Click,Cost = line.split('\t')
+		elif check_line_item(line) == 6:
+			PV_ID,Account_ID,KeyWord,QueryWord,Style_ID,Click,Cost = repair_line_6(line)
+		sheet.write(row,y+8,PV_ID)
+		sheet.write(row,y+9,Account_ID)
+		sheet.write(row,y+10,KeyWord.decode('gbk', "ignore"))
+		sheet.write(row,y+11,QueryWord.decode('gbk', "ignore"))
+		sheet.write(row,y+12,Style_ID)
+		sheet.write(row,y+13,int(Click))
+		sheet.write(row,y+14,int(Cost))
+	print "origin data is saved into EXCEL"
+
+def write_QuerySum(a,b,sheet,x,y):
+	row = x
+	#创建表头
+	sheet.write(row,y,'ExpQueryWord')
+	sheet.write(row,y+1,'ExpPV')
+	sheet.write(row,y+2,'ExpClick')
+	sheet.write(row,y+3,'ExpCost')
+	sheet.write(row,y+5,'ComQueryWord')
+	sheet.write(row,y+6,'ComPV')
+	sheet.write(row,y+7,'ComClick')
+	sheet.write(row,y+8,'ComCost')
+	
+	#先写左侧实验组数据
+	for key in a:
+		row += 1
+		sheet.write(row,y,key.decode('gbk', "ignore"))
+		sheet.write(row,y+1,a[key][0])
+		sheet.write(row,y+2,a[key][2])
+		sheet.write(row,y+3,a[key][3])
+
+		#再写右侧对照组数据
+	row = x
+	for key in b:
+		row += 1
+		sheet.write(row,y+5,key.decode('gbk', "ignore"))
+		sheet.write(row,y+6,b[key][0])
+		sheet.write(row,y+7,b[key][2])
+		sheet.write(row,y+8,b[key][3])
+	
+	print "QuerySum data is saved into EXCEL"
+
+def write_ValidData(a,sheet):
+	row = 0
+	sheet.write(row,0,'QueryWord')
+	sheet.write(row,1,'ExpPV')
+	sheet.write(row,2,'ExpClick')
+	sheet.write(row,3,'ExpCost')
+	sheet.write(row,4,'ComPV')
+	sheet.write(row,5,'ComClick')
+	sheet.write(row,6,'ComCost')
+	sheet.write(row,7,'Style_ID')
+	for key in a:
+		row += 1
+		sheet.write(row,0,key.decode('gbk', "ignore"))
+		sheet.write(row,1,a[key][0])
+		sheet.write(row,2,a[key][2])
+		sheet.write(row,3,a[key][3])
+		sheet.write(row,4,a[key][4])
+		sheet.write(row,5,a[key][6])
+		sheet.write(row,6,a[key][7])
+		sheet.write(row,7,a[key][1])
+	
+	print "ValidData data is saved into EXCEL"
+
+def write_result(a,sheet,b,shunxu):
+	#写表头
+	row = 0
+	sheet.write(row,0,'Style')
+	sheet.write(row,1,'ExpPV')
+	sheet.write(row,2,'ExpClick')
+	sheet.write(row,3,'ExpCost')
+	sheet.write(row,4,'ExpCTR')
+	sheet.write(row,5,'ComPV')
+	sheet.write(row,6,'ComClick')
+	sheet.write(row,7,'ComCost')
+	sheet.write(row,8,'ComCTR')
+	sheet.write(row,9,'CTR_change')
+	
+	for key in shunxu:
+		row += 1
+		sheet.write(row,0,b[shunxu[key]])
+		sheet.write(row,1,a[shunxu[key]][0])
+		sheet.write(row,2,a[shunxu[key]][1])
+		sheet.write(row,3,a[shunxu[key]][2])
+		sheet.write(row,4,a[shunxu[key]][6])
+		sheet.write(row,5,a[shunxu[key]][3])
+		sheet.write(row,6,a[shunxu[key]][4])
+		sheet.write(row,7,a[shunxu[key]][5])
+		sheet.write(row,8,a[shunxu[key]][7])
+		sheet.write(row,9,a[shunxu[key]][8])
+	
+	print "ALL is successfully done"
---
+++ analysis.py
@@ -0,0 +1,60 @@
+﻿# -*- coding:utf-8 -*-
+import OTA
+import pyExcelerator
+#每次处理，请修改fileAB name
+fileA_name = 'expr_account_key.with_click.20160715'
+fileB_name = 'comp_account_key.with_click.20160715'
+
+fileA = open(fileA_name)
+fileB = open(fileB_name)
+
+#对文件处理，输出为字典
+dicA = OTA.convert2dic(fileA)
+print "Data of Experiment is loaded"
+dicB = OTA.convert2dic(fileB)
+print "Data of Control is loaded"
+
+#求取交集，实现vlookup功能，记录在字典中
+dicC = OTA.dic_intersection(dicA,dicB)
+
+#定义样式对照表
+dicE = {"11":u"酒店-地域酒店","12":u"酒店-通用词","13":u"酒店-单体品牌词","14":u"酒店-连锁品牌词","21":u"旅游-单线路","22":u"旅游-通用词","31":u"机票通用词","32":u"机票-交通出行聚合","96":u"机票","97":u"酒店","98":u"旅游","99":u"汇总"}
+
+#有机票请用这个
+dicF = {1:"13",2:"11",3:"14",4:"21",5:"22",6:"32",7:"31",8:"96",9:"97",10:"98",11:"99"}
+
+'''
+#没机票用这个
+dicF = {1:"13",2:"11",3:"14",4:"21",5:"22",9:"97",10:"98",11:"99"}
+'''
+
+#汇总数据
+dicD = OTA.calculate_result(dicC)
+
+
+workbook = pyExcelerator.Workbook()
+sheet1 = workbook.add_sheet('origin')
+sheet2 = workbook.add_sheet('QuerySum')
+sheet3 = workbook.add_sheet('ValidData')
+sheet4 = workbook.add_sheet('result')
+
+#重新打开文件
+fileA = open(fileA_name)
+fileB = open(fileB_name)
+
+#左侧为实验组，右侧为对照组,写入原始数据
+OTA.write_origin(fileA,fileB,sheet1,0,0)
+
+#写入汇总文件
+OTA.write_QuerySum(dicA,dicB,sheet2,0,0)
+
+#写入有效vlookup数据
+OTA.write_ValidData(dicC,sheet3)
+
+#写入结果
+OTA.write_result(dicD,sheet4,dicE,dicF)
+
+#寻找合适的命名
+Final_name = fileA_name[-8:]+'.xls'
+
+workbook.save(Final_name)
